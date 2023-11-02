4b9f7883259d9a5c8dd6bbc724f3bad625b2fcbe
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2023-11-02 23:32:16 +0800

    use chrome.storage

--- src/background/index.ts
+++ src/background/index.ts
@@ -12,15 +12,28 @@ chrome.runtime.onInstalled.addListener(async (opt) => {
 })
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+  if (request.message === 'check_access_token') {
+    chrome.storage.local.get('google_auth_token').then(result => {
+      if (result.google_auth_token) {
+        sendResponse({ status: 'success' })
+      } else {
+        sendResponse({ status: 'failed' })
+      }
+    })
+  }
+
+
   if (request.message === 'get_access_token') {
     chrome.identity.getAuthToken({ interactive: true }, (token) => {
-      console.log(token)
-      sendResponse({ data: token })
+      sendResponse({ status: 'success' })
+      chrome.storage.local.set({ 'google_auth_token': token })
     })
   }
 
   if (request.message === 'get_events') {
-    chrome.identity.getAuthToken({ interactive: true }, (token) => {
+    chrome.storage.local.get('google_auth_token').then(result => {
+      const token = result.google_auth_token
       const currentDate = new Date()
       const timeMin = currentDate.toISOString()
       currentDate.setHours(currentDate.getHours() + 5);
@@ -34,7 +47,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
 
       fetch(url, config).then(r => r.json()).then(data => {
-        sendResponse(data)
+        sendResponse({ status: 'success', data })
       })
     })
   }
