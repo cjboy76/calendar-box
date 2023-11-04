d6a9b70cc3a17700e6c187bc4dfb8bba01d12e20
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2023-11-04 12:52:34 +0800

    Create progress bar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps({
+  loading: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <div class="w-full h-0.5 flex items-center relative">
+    <div
+      v-show="loading"
+      class="progress"
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+.progress {
+  width: 20%;
+  height: 100%;
+  top: 0;
+  background-color: white;
+  position: absolute;
+  animation: move 1.5s linear infinite;
+}
+
+@keyframes move {
+  0% {
+    left: -100%;
+  }
+  100% {
+    left: 100%;
+  }
+}
+</style>
